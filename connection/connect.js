21b22ebe8e676fa89bb49f4f0522b4e84564914b
DhamaniSaranya <dhamanisaranya69@gmail.com>
2022-05-26 22:27:27 +0530

    make it better

--- connection/connect.js
+++ connection/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function main() {
   await mongoose.connect(
     // 'mongodb+srv://saurav-dope:Saurav_6989@cluster0.ztxtp.mongodb.net/instaclone?retryWrites=true&w=majority'
-    'mongodb+srv://DhamaniSaranya:Dhamani@1997@cluster0.howsj.mongodb.net/instaclone?retryWrites=true&w=majority', ()=>{
+    'mongodb+srv://DhamaniSaranya:Dhamani@1997@cluster0.howsj.mongodb.net/instaclone?retryWrites=true&w=majority', ()=> {
        console.log("mongoose connected")
     });
 }
